3040d6f7903560bcde328fede4fb427057e495ed
niqiuki <sashadekt1012.em@gmail.com>
2024-12-14 22:20:18 +0200

    swagger added

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const { errorHandler } = require("./src/middleware/errorMiddleware");
 const { sequelize } = require("./models");
 const cors = require("cors");
 const session = require("express-session");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./src/swagger");
+const swaggerDocs = require("./src/swagger")
 
 const  corsOptions = {
   credentials:true,
@@ -24,10 +27,12 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/users", require("./src/routes/userRoutes"));
 
 app.use(errorHandler);
+
+swaggerDocs(app);
 
 sequelize
   .authenticate()
@@ -35,8 +40,11 @@ sequelize
     console.log("Database connected successfully.");
     app.listen(port, () => {
       console.log(`Server started on port ${port}`);
+      console.log(`Swagger UI is available http://localhost:${port}/api-docs`);
     });
   })
   .catch((err) => {
     console.error("Error connecting to the database:", err);
   });
+
+  
