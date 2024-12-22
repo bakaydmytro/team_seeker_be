0eadebab8fd28a5aea255c8348677e19b25729d8
dmytro_bakai <dbaka@softserveinc.com>
2024-12-22 21:13:42 +0200

    Resolve merge conflicts

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -248,7 +248,7 @@ const getRecentlyPlayedGames = async (req, res) => {
     console.error(error);
     res.status(500).json({ message: "Failed to fetch games", error });
   }
-});
+};
 
 const searchUsers = asyncHandler(async (req, res) => {
   try {
