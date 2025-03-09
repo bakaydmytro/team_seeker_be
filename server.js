72d73aefac32a85672e337b2510209f4d95ecc25
dmytro_bakai <dbaka@softserveinc.com>
2025-03-10 00:10:27 +0200

    Fix deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.API_PORT || 5000;
+const port = process.env.API_PORT || 8080;
 const { errorHandler } = require("./src/middleware/errorMiddleware");
 const { sequelize, User, Chat, Message } = require("./models");
 const cors = require("cors");
@@ -17,7 +17,7 @@ const path = require("path");
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "*", methods: ["GET", "POST"] }
+  cors: { origin: "*", methods: ["GET", "POST"] },
 });
 
 app.use(
@@ -39,9 +39,8 @@ app.use("/uploads/avatars", express.static("uploads"));
 app.use(errorHandler);
 app.get("/generate-json", async (req, res) => {
   try {
-
     const users = await User.findAll();
-    const usersJson = users.map(user => user.toJSON()); 
+    const usersJson = users.map((user) => user.toJSON());
     const filePath = path.join(__dirname, "users.json");
 
     fs.writeFileSync(filePath, JSON.stringify(usersJson, null, 2), "utf-8");
@@ -59,9 +58,9 @@ app.get("/generate-json", async (req, res) => {
 });
 app.get("/users", async (req, res) => {
   try {
-    const users = await User.findAll(); 
-    const usersJson = users.map(user => user.toJSON()); 
-    res.json(usersJson); 
+    const users = await User.findAll();
+    const usersJson = users.map((user) => user.toJSON());
+    res.json(usersJson);
   } catch (error) {
     console.error(error);
     res.status(500).send("Unable to get users");
@@ -69,9 +68,8 @@ app.get("/users", async (req, res) => {
 });
 swaggerDocs(app);
 
-
 io.use((socket, next) => {
-  const token = socket.handshake.query.token; 
+  const token = socket.handshake.query.token;
 
   if (!token) {
     return next(new Error("Authentication error: Token is missing"));
@@ -81,18 +79,17 @@ io.use((socket, next) => {
     if (err) {
       return next(new Error("Authentication error: Invalid token"));
     }
-    
+
     socket.user = decoded;
     next();
   });
 });
 
 io.on("connection", (socket) => {
-  console.log(`User connected: ${socket.user.id}`); 
+  console.log(`User connected: ${socket.user.id}`);
 
-  socket.on('joinChat', async ({ chat_id }) => {
-
-    const userId = socket.user.id; 
+  socket.on("joinChat", async ({ chat_id }) => {
+    const userId = socket.user.id;
     const chat = await Chat.findByPk(chat_id, {
       include: {
         model: User,
@@ -102,22 +99,22 @@ io.on("connection", (socket) => {
     });
 
     if (!chat) {
-      socket.emit('chatError', 'Chat not found or access denied.');
+      socket.emit("chatError", "Chat not found or access denied.");
       return;
     }
 
     socket.join(`chat_${chat_id}`);
-    socket.to(`chat_${chat_id}`).emit('userJoined', { userId });
+    socket.to(`chat_${chat_id}`).emit("userJoined", { userId });
   });
 
-  socket.on('sendMessage', async ({ chat_id, content }) => {
-    const userId = socket.user.id; 
+  socket.on("sendMessage", async ({ chat_id, content }) => {
+    const userId = socket.user.id;
 
     if (!chat_id || !content) {
-      socket.emit('messageError', 'Chat ID and content are required.');
+      socket.emit("messageError", "Chat ID and content are required.");
       return;
     }
-  
+
     const currentTime = Date.now();
     const oneMinuteAgo = currentTime - 60000;
 
@@ -130,15 +127,24 @@ io.on("connection", (socket) => {
     });
 
     if (recentMessages >= 20) {
-      socket.emit('messageError', 'You are sending messages too quickly. Please slow down.');
+      socket.emit(
+        "messageError",
+        "You are sending messages too quickly. Please slow down."
+      );
       return;
     }
 
     const repeatedWordPattern = /\b(\w+)\b(?:.*\b\1\b){6,}/;
     const maxMessageLength = 500;
 
-    if (repeatedWordPattern.test(content) || content.length > maxMessageLength) {
-      socket.emit('messageError', 'Your message looks like spam or is too long.');
+    if (
+      repeatedWordPattern.test(content) ||
+      content.length > maxMessageLength
+    ) {
+      socket.emit(
+        "messageError",
+        "Your message looks like spam or is too long."
+      );
       return;
     }
 
@@ -148,27 +154,29 @@ io.on("connection", (socket) => {
         where: { id: userId },
         through: { attributes: [] },
       },
-   });
+    });
 
     if (!chat) {
-      socket.emit('chatError', 'Chat not found or access denied.');
+      socket.emit("chatError", "Chat not found or access denied.");
       return;
     }
 
-    const message = await Message.create({ chat_id, sender_id: userId, content });
+    const message = await Message.create({
+      chat_id,
+      sender_id: userId,
+      content,
+    });
 
-    io.to(`chat_${chat_id}`).emit('newMessage', {
+    io.to(`chat_${chat_id}`).emit("newMessage", {
       id: message.id,
       senderId: message.sender_id,
       content: message.content,
       createdAt: message.createdAt,
     });
-
   });
 
-  socket.on('leaveChat', async ({ chat_id }) => {
-
-    const userId = socket.user.id; 
+  socket.on("leaveChat", async ({ chat_id }) => {
+    const userId = socket.user.id;
     const chat = await Chat.findByPk(chat_id, {
       include: {
         model: User,
@@ -178,11 +186,11 @@ io.on("connection", (socket) => {
     });
 
     if (!chat) {
-      socket.emit('chatError', 'Chat not found or access denied.');
+      socket.emit("chatError", "Chat not found or access denied.");
       return;
     }
     socket.leave(`chat_${chat_id}`);
-    socket.to(`chat_${chat_id}`).emit('userLeft', { userId });
+    socket.to(`chat_${chat_id}`).emit("userLeft", { userId });
   });
 
   socket.on("disconnect", () => {
@@ -196,7 +204,9 @@ sequelize
     console.log("Database connected successfully.");
     server.listen(port, () => {
       console.log(`Server started on port ${port}`);
-      console.log(`Swagger UI is available at http://localhost:${port}/api-docs`);
+      console.log(
+        `Swagger UI is available at http://localhost:${port}/api-docs`
+      );
     });
   })
   .catch((err) => {
